8cbe4b41aa133922071ca0ceba5dc41ce2e98c54
safinhr <hrsafin2434@gmail.com>
2024-05-11 00:27:41 +0600

    type operator

--- Mission 2/5.6($exists, $Type, $size).js
+++ Mission 2/5.6($exists, $Type, $size).js
@@ -5,3 +5,13 @@
 db.test.find({age : {$exists : true}})
 
 db.test.find({ phone : {$exists : false}})
+
+db.test.find({unknown : {$exists : false}})
+db.test.find({ company : {$exists : true}})
+
+
+// $type operator
+
+db.test.find({ age : {$type : "string"}})
+
+db.test.find({ age : {$type : "number"} })
