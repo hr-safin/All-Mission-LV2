d22f233e5ef5e03a85776c99f180c47d13d80e00
safinhr <hrsafin2434@gmail.com>
2024-05-10 23:31:52 +0600

    explicit and operator

---
+++ Mission 2/5.5($and, $or, Implicit Vs Explicit).js
@@ -0,0 +1,14 @@
+// $And --> $and performs a logical AND operation on an array of one or more expressions (<expression1>, <expression2>, and so on) and selects the documents that satisfy all the expressions.
+
+db.test.find({age : {$gte : 18, $lt : 32}}, {age : 1}).sort({age : 1})
+
+// Explicit And operator
+
+// find all the data not equal to 15 and less than 35 from json data
+
+db.test.find({
+    $and : [
+        {age : {$ne : 15}},
+        {age : {$lt : 35}}
+    ]
+}, {age : 1}).sort({age : 1})
