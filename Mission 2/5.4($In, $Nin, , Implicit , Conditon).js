f7d52bb152fb6bf4fcc37f0bf2263a3297a559b2
safinhr <hrsafin2434@gmail.com>
2024-05-10 23:01:30 +0600

    uses

--- Mission 2/5.4($In, $Nin, , Implicit , Conditon).js
+++ Mission 2/5.4($In, $Nin, , Implicit , Conditon).js
@@ -1,7 +1,13 @@
 // $In --> The $in operator selects the documents where the value of a field equals any value in the specified array.
 
-// find all the data less than 18 : 
-db.test.find({age : {$lt :  18}}, {age : 1}) // ---> field filtering for specific data
+// find all the data less than 18 :
+db.test.find({ age: { $lt: 18 } }, { age: 1 }); // ---> field filtering for specific data
 
-// find all the data greater than 18 and less than 30 : 
-db.test.find({age : {$gt : 18, $lt : 30}}, {age : 1}).sort({age  : 1}) // implicit and
+// find all the data greater than 18 and less than 30 :
+db.test.find({ age: { $gt: 18, $lt: 30 } }, { age: 1 }).sort({ age: 1 }); // implicit and
+db.test
+  .find({ gender: "Female", age: { $gt: 18, $lt: 30,} }, { age: 1, gender: 1 })
+  .sort({ age: 1 }); // implicit and
+db.test
+  .find({ gender: "Female", age: {$in : [18,20,22,24,26,28,30]} }, { age: 1, gender: 1 })
+  .sort({ age: 1 }); // implicit and
