dcd008f53fe077edc7c20accfa3061c884730bad
safinhr <hrsafin2434@gmail.com>
2024-05-11 16:10:23 +0600

    push operator for duplicating the value

--- Mission 2/5.8($Set, $AddToSet , $Push).js
+++ Mission 2/5.8($Set, $AddToSet , $Push).js
@@ -21,14 +21,24 @@ db.test.updateOne(
  )
 
 
- // $addToSet -- > 
+ // $addToSet -- >  will not duplicate any value
 
  db.test.updateOne(
     { "_id" : ObjectId("6406ad65fc13ae5a400000bf")},
     {
         $addToSet : {
-            interests : { $each :  ["Cokking", "Pubg"] 
+            interests : { $each : [ "Cooking", "PUBG", "Valorant"]}
         }
-     },
-    }
+    },
+ )
+
+ // $push --- duplicate
+
+ db.test.updateOne(
+    { "_id" : ObjectId("6406ad65fc13ae5a400000bf")},
+    {
+        $push : {
+            interests : { $each : [ "Cooking", "PUBG", "Valorant"]}
+        }
+    },
  )
