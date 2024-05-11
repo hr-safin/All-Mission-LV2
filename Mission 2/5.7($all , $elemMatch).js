250235eb3fa87ca06776a223c15d1903ccfffcd0
safinhr <hrsafin2434@gmail.com>
2024-05-11 13:29:10 +0600

    elemMatch

--- Mission 2/5.7($all , $elemMatch).js
+++ Mission 2/5.7($all , $elemMatch).js
@@ -1,8 +1,37 @@
 // $All and $elemMatch
 
-db.test.find({ interests : "Cooking"}).project({ interests : 1})
-db.test.find({ "interests.2" : "Cooking"}).project({ interests : 1})
+db.test.find({ interests: "Cooking" }).project({ interests: 1 });
+db.test.find({ "interests.2": "Cooking" }).project({ interests: 1 });
 
 // find the travel, gaming , reading from the json data
+// $ALL
 
-db.test.find( {interests : {$all : ["Travelling", "Gaming", "Reading"]}} ).project( {interests : 1} )
+db.test
+  .find({ interests: { $all: ["Travelling", "Gaming", "Reading"] } })
+  .project({ interests: 1 });
+
+// find javascript from json data
+
+db.test.find({ "skills.name": "JAVASCRIPT" }).project({ skills: 1 });
+
+db.test
+  .find({
+    skills: {
+      name: "JAVASCRIPT",
+      level: "Intermidiate",
+      isLearning: false,
+    },
+  })
+  .project({ skills: 1 });
+
+
+  // $elemMatch
+
+  db.test.find({
+    skills : {
+        $elemMatch : {
+            name : "JAVASCRIPT",
+            level : "Intermidiate"
+        }
+    }
+  }).project( { skills : 1} )
