8981d99beeea572d78ed87721e0a13e9b2fa4d26
safinhr <hrsafin2434@gmail.com>
2024-05-11 19:10:28 +0600

    pull and pullall operator

--- Mission 2/5.9($unset, $Pop, $pull, $pullAll).js
+++ Mission 2/5.9($unset, $Pop, $pull, $pullAll).js
@@ -27,3 +27,26 @@ db.test.updateOne(
         $pop : { friends : -1}
     }
 )
+
+// $pull --> remove one value from an array
+
+db.test.updateOne(
+    {_id : ObjectId("6406ad65fc13ae5a400000bf")},
+    {
+        $pull : { friends : "Jhankar Mahbub"}
+    }
+)
+
+// $pullAll ---> remove multiple elements from an array
+
+db.test.updateOne(
+    {_id : ObjectId("6406ad65fc13ae5a400000bf")},
+    {
+        $pullAll : { interests : ["Cooking",
+		"PUBG",
+		"Valorant",
+		"Cooking",
+		"PUBG",
+		"Valorant"]}
+    }
+)
