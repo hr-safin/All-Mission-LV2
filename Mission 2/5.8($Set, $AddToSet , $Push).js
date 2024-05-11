d9d88713cd399453b2a508e770e025d9a15c5362
safinhr <hrsafin2434@gmail.com>
2024-05-11 16:04:50 +0600

    addToset and each operator

--- Mission 2/5.8($Set, $AddToSet , $Push).js
+++ Mission 2/5.8($Set, $AddToSet , $Push).js
@@ -1,10 +1,34 @@
 // update 
 // $set ---> the set operator replaces the value of a field with the specified value
-db.products.updateOne(
+db.test.updateOne(
     { "_id" : ObjectId("6406ad65fc13ae5a400000bf")},
     { $set:
        {
          age : 54
        }
+    }
+ )
+
+ db.test.updateOne(
+    { "_id" : ObjectId("6406ad65fc13ae5a400000bf")},
+    {
+        $set : {
+            gender : "Male",
+            company : "Programming Hero",
+            interests : ["Gaming"]
+        }
+    }
+ )
+
+
+ // $addToSet -- > 
+
+ db.test.updateOne(
+    { "_id" : ObjectId("6406ad65fc13ae5a400000bf")},
+    {
+        $addToSet : {
+            interests : { $each :  ["Cokking", "Pubg"] 
+        }
+     },
     }
  )
