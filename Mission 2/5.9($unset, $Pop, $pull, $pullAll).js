d5ac5a89223ea2e2e0d9be02c0c5185e31bdc5ff
safinhr <hrsafin2434@gmail.com>
2024-05-11 19:03:36 +0600

    pop operator

--- Mission 2/5.9($unset, $Pop, $pull, $pullAll).js
+++ Mission 2/5.9($unset, $Pop, $pull, $pullAll).js
@@ -7,3 +7,23 @@ db.test.updateOne(
       $unset : {age : ""}
     }
 )
+
+// $pop --> it is used to remove last element of an array
+
+// remove the last element from an array by using $pop
+
+db.test.updateOne(
+    {_id : ObjectId("6406ad65fc13ae5a400000bf")},
+    {
+        $pop : { friends : 1}
+    }
+)
+
+// remove the first element from an array by using $pop
+
+db.test.updateOne(
+    {_id : ObjectId("6406ad65fc13ae5a400000bf")},
+    {
+        $pop : { friends : -1}
+    }
+)
