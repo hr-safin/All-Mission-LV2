6cf109a24a0ad35df54594169a6c81a5159b54a1
safinhr <hrsafin2434@gmail.com>
2024-05-13 23:05:38 +0600

    comaprison operator : gt, gte, eq, ne, lt , lte

--- Mission 2/practice2.js
+++ Mission 2/practice2.js
@@ -1,2 +1,49 @@
 // MongoDb practice
 
+// $eq --> equality operator
+
+db.test.find(
+    {
+        age : {$eq : 20}
+    }
+)
+
+// $gt --> greater than
+
+db.test.find({
+    age : {
+        $gt : 20
+    }
+})
+
+// $gte --> greater than or equal to
+
+db.test.find({
+    age : {
+        $gte : 20 
+    }
+})
+
+// $le --> less than
+
+db.test.find({
+    age : {
+        $lt : 20
+    }
+})
+
+// $lte --> less than or equal to 
+
+db.test.find({
+    age : {
+        $lte  :  22
+    }
+})
+
+//$ne --> not equal to
+
+db.test.find({
+    age : {
+        $ne : 17
+    }
+})
