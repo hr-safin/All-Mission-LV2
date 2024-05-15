bb84c05bdb010d47c8642ac7018225b456eeb85a
safinhr <hrsafin2434@gmail.com>
2024-05-15 23:23:48 +0600

    elemMatch operator

--- Mission 2/practice3.js
+++ Mission 2/practice3.js
@@ -1,1 +1,5 @@
-//$elem match
+//$elemMatch operator --> The $elemMatch operator in MongoDB is used to filter documents based on conditions within an array field.
+
+db.products.find({
+    tags: { $elemMatch: { category: "electronics" } }
+  })
