a1931b4f7e9ac89c69eff265b5b6c9d808b9c649
safinhr <hrsafin2434@gmail.com>
2024-05-15 23:46:09 +0600

    set

--- Mission 2/practice3.js
+++ Mission 2/practice3.js
@@ -14,3 +14,10 @@ db.collection.update(
     { $addToSet: { item : "h" } }
   )
 
+// $set --> $set operator is a fundamental tool for updating and creating fields within documents.
+
+db.collection.update(
+    { _id: "3434" }, 
+    { $set: { age: 23 } }
+  )
+
