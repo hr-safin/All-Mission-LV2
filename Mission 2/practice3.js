a729e0cce41b9013b72dee0c97f2b59bab9aff2d
safinhr <hrsafin2434@gmail.com>
2024-05-17 23:35:52 +0600

    pull operator revised

--- Mission 2/practice3.js
+++ Mission 2/practice3.js
@@ -91,6 +91,10 @@ db.users.updateOne(
   {id : 234214},
   {$pull : {items : {product_id : 23423}}}
 )
+db.users.updateOne(
+  {id : 234214},
+  {$pull : {items : {product_id : 23423}}}
+)
 
 // pullAll --> the $pullAll operator is specifically designed to remove all occurrences of specified values from an array field within a document.
 
