91a3b60a307ef465576bda85617ba85c4e62b38d
safinhr <hrsafin2434@gmail.com>
2024-07-28 11:20:13 +0600

    basic data types

--- Mission 1/module1/src/1.4(basicDataTypes).ts
+++ Mission 1/module1/src/1.4(basicDataTypes).ts
@@ -2,27 +2,36 @@
 
 //string
 let fruits : string = "Apple"
+let fruits2 : string = "Orange"
 
 //number
 let number : number = 12
+let number2 : number = 12
 
 //boolean
 let isAdmin : boolean = true
+let isAdmin2 : boolean = true
 
 //undefined
 let x : undefined = undefined
+let x2 : undefined = undefined
 
 //null
 let y : null = null
+let y2 : null = null
 
 let d : number ;
 
 d= 123
 
+let c : string;
+c="New Jersey Institute Of technology"
+
 
 //array
-
 let arrayOfNumber : number[]  = [12,3,4]
+let arrayOfNumber2 : number[] = [1,23,4]
+arrayOfNumber2.push(19)
 
 let arrayOfName : string[] = ["safin", "hasibur", "rahman safin"]
 
@@ -33,6 +42,9 @@ arrayOfName.push("munira")
 let ageOfNumber : [number, string, boolean] = [34, "Hasibur rahman safin", true]
 
 ageOfNumber[0] = 44
+
+let info : [ string, string, number] = ["safin", "njit", 2024]
+info[0] = "hasibur rahman safin"
 
 // console.log(ageOfNumber)
 
