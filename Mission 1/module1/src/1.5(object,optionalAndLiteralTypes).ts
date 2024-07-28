995b028a63312bd2466099dedb8837289bfb169c
safinhr <hrsafin2434@gmail.com>
2024-07-28 19:26:34 +0600

    object in typescript

--- Mission 1/module1/src/1.5(object,optionalAndLiteralTypes).ts
+++ Mission 1/module1/src/1.5(object,optionalAndLiteralTypes).ts
@@ -13,5 +13,46 @@
         lastName : "Shafin",
         age :  23
     }
+
+
+
+
+    const info : {
+        name : string
+        goal : string
+        dreamSchool : "New Jersey Institute Of Technology" // Literal Type or fixed Type
+        age? : number  // Optional Type
+
+    } = {
+        name : "Hasibur rahman safin",
+        goal : "To Become a software engineer",
+        dreamSchool : "New Jersey Institute Of Technology"
+     }
+
+
+
+     info.goal = "I want to go to usa and become a software engineer there"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
+
+
+
+
+
+
+
 
