870c202109465e952b43e1fa4386f377bfa193f5
safinhr <hrsafin2434@gmail.com>
2024-07-29 20:49:40 +0600

    nullable and unknown type

--- Mission 1/module1/src/1.12(Never,unknown,nullable).ts
+++ Mission 1/module1/src/1.12(Never,unknown,nullable).ts
@@ -14,29 +14,63 @@ const searchName = (value : string | null) => {
 }
 
 // searchName(null)
+const collegeName = (value : string | null) => {
+    if(value){
+        console.log(`You are going to study at ${value} `)
+    }
+    else{
+        console.log("you are not going to study")
+    }
+}
+
+collegeName("New Jersey Institute Of Technology")
+collegeName(null)
 
 
 // Unknown Type
 
-const getSpeedInMeterPerSecond = (value : unknown) => {
+// const getSpeedInMeterPerSecond = (value : unknown) => {
 
-    if(typeof value === "number"){
-        const meterPerSecond = (value * 1000) / 3600
-        console.log(`The value is ${meterPerSecond} ms^-1`) 
+//     if(typeof value === "number"){
+//         const meterPerSecond = (value * 1000) / 3600
+//         console.log(`The value is ${meterPerSecond} ms^-1`) 
+//     }
+
+//     else if(typeof value === "string"){
+//         const [result, unit] = value.split(" ")
+//         const meterPerSecond = (parseFloat(result) * 1000) / 3600
+//         console.log(`The value is ${meterPerSecond} ms^-1`)
+//     }
+//     else{
+//         console.log("Wrong Input")
+//     }
+// }
+
+
+
+
+// getSpeedInMeterPerSecond(null)
+
+const getMeterPerSecond = (value : unknown) => {
+    if( typeof value === "number"){
+        const convertedSpeed  = (value * 1000) / 3600
+        console.log(`The result is ${convertedSpeed} km^-1`) 
     }
-
     else if(typeof value === "string"){
-        const [result, unit] = value.split(" ")
-        const meterPerSecond = (parseFloat(result) * 1000) / 3600
-        console.log(`The value is ${meterPerSecond} ms^-1`)
+        const numberArr = value.split(" ")
+        const [result, unit] = numberArr
+        const convertedSpeed  = (parseFloat(result) * 1000) / 3600
+        console.log(`The result is ${convertedSpeed} km^-1`)  
     }
+
     else{
-        console.log("Wrong Input")
+        console.log("You have given a wrong input")
     }
 }
 
-
-getSpeedInMeterPerSecond(null)
+getMeterPerSecond("1000 km^-1")
+getMeterPerSecond(1000)
+getMeterPerSecond(null)
 
 // Never --> nothing will happen or run
 
