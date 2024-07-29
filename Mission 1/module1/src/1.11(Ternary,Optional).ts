a8d456253ce51e33dc6926a1e215ed786640a3f9
safinhr <hrsafin2434@gmail.com>
2024-07-29 17:40:24 +0600

    Ternary optional and nullish

--- Mission 1/module1/src/1.11(Ternary,Optional).ts
+++ Mission 1/module1/src/1.11(Ternary,Optional).ts
@@ -13,21 +13,24 @@
 
 
     // ternary operator
-
-    const isAdult = age >= 18 ? "Adult" : "NOT Adult"
-
-    // console.log(isAdult)
-
+   
+    const mark : number = 50
+    const passOrFail  = mark >=40 ? "You are passed" : "You are failed"
+    
+    console.log(passOrFail)
 
     // nullish coalescing = null and undefined
     // null & undefined --> decision making
 
     const isAuthenticated = ""
+    const isAdmin = ""
 
     const result1 = isAuthenticated ?? "Guest"
+    const prnt1 = isAdmin ?? "Yes"
     const result2 = isAuthenticated ? isAuthenticated : "Guest"
+    const prnt2 = isAdmin ? isAdmin : "No"
 
-    // console.log({result1}, {result2})
+    console.log({prnt1}, {prnt2})
 
 
     // optional Chaining
@@ -60,6 +63,37 @@
     const permanentAddress = user?.address?.permanentAddress ?? "No permanent address"
 
     console.log(permanentAddress)
+
+    type BioData = {
+        name : "Hasibur Rahman Safin",
+        age : number
+        college : string
+        address : {
+            road? : number
+            block : string
+            area : string
+            state : "Dhaka"
+        }
+    }
+
+
+    const bioData : BioData = {
+        name : "Hasibur Rahman Safin",
+        age : 24,
+        college : "New Jersey Institute Of Technology",
+        address : {
+            block : "B",
+            area : "Mohanagar Project",
+            state : "Dhaka"
+        }
+    }
+
+
+    const areaName =  bioData?.address?.area
+    const roadName = bioData?.address?.road ?? "No Road Number"
+
+    console.log({areaName}, {roadName})
+
 
 
 
