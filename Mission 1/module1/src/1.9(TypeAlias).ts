3872da04f6bae3b05229b71f376a2fe201118041
safinhr <hrsafin2434@gmail.com>
2024-07-29 16:35:30 +0600

    Type Alias

--- Mission 1/module1/src/1.9(TypeAlias).ts
+++ Mission 1/module1/src/1.9(TypeAlias).ts
@@ -37,8 +37,41 @@
     const add : Add = (num1, num2) =>  num1 + num2
     
 
+    type User =  {
+        name : string
+        age : number
+        college : string
+        program : string
+    }
 
 
+    const user1 : User  = {
+        name : "Hasibur Rahman Safin",
+        age : 24,
+        college : "New Jersey Institute Of Technology",
+        program : "Computer Science" 
+    }
+
+
+    const user2 : User = {
+        name : "Nur Al Munira Mahi",
+        age : 23,
+        college : "Montclair State University",
+        program : "Business Analytics"
+    }
+
+
+    type UserName2 = string
+
+    const userName2 : UserName2 = "Hasibur rahman Safin"
+    
+    type IsAd = boolean
+    const isAdmin2 : IsAd = true
+
+    type Addition = (x : number, y : number) => number
+
+
+    const addition : Addition = (num1, num2) => num1 + num2
 
 
 
