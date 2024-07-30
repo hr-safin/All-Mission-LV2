a6f8f12a2df6c2f8eee543aab616e1b7f1109465
safinhr <hrsafin2434@gmail.com>
2024-07-30 23:53:49 +0600

    interface practied

--- Mission 1/module2/src/practice2.ts
+++ Mission 1/module2/src/practice2.ts
@@ -10,6 +10,29 @@ let len : number = (str as string).length
 console.log(len)
 
 
+// Interface
+
+interface Rectangle {
+    height : number;
+    weight : number
+}
+
+type Rectangle2 = {
+    height : number;
+    weight : number
+}
+
+interface ColoredRectangle extends Rectangle2{
+    color : string
+}
+
+const rectangle : ColoredRectangle = {
+  height : 12,
+  weight : 20,
+  color : "Blue"
+}
+
+
 
 
 
