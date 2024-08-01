32f93b5e59ae19ccfa76ebc583d39ba08aec2b0b
safinhr <hrsafin2434@gmail.com>
2024-08-02 00:08:02 +0600

    constraints key of

--- Mission 1/module2/src/2.7(constraintsKey).ts
+++ Mission 1/module2/src/2.7(constraintsKey).ts
@@ -14,12 +14,29 @@
 
     const owner : Owner2 = "car"
 
+    
+
+    const  getPropertyValues =<X, Y extends keyof X>(obj : X, key : Y) =>{
+        return obj[key]
+    }
+
     const user = {
 
-        name : "Hasibur Rahman Safin",
+        name1 : "Hasibur Rahman Safin",
         email : "helloWortk@gmail.com",
         id : 2323
     }
+
+    const car = {
+        brand : "Lamborgini",
+        model : "2013"
+    }
+
+
+    const result1 = getPropertyValues(user, "name1")
+    const result2 = getPropertyValues(car, "brand")
+
+    
 
 
 
