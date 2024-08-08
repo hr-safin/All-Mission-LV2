b9184e872c8fcd6d9c2c6e9b2fb02819bd71e087
safinhr <hrsafin2434@gmail.com>
2024-08-08 19:30:38 +0600

    revise :interface

--- Mission 1/module2/src/practice3.ts
+++ Mission 1/module2/src/practice3.ts
@@ -14,6 +14,59 @@
     console.log(typeof typeNum)
 
 
+    // Interface 
+
+    interface Shape{
+        name : string,
+        color : string,
+        area() : number
+    }
+
+    const circle : Shape = {
+        name : "circle",
+        color : "Red",
+        area() {
+            return parseFloat((Math.PI * 2 * 2).toFixed())
+        }
+    }
+
+    const circleFunction = (Shape : Shape) : void => {
+        console.log(`Name of the shape ${Shape.name}`)
+        console.log(`The area of the circle : ${Shape.area()}`)
+    }
+
+    circleFunction(circle)
+
+
+    type Hello = {
+        hello : string
+    }
+
+    type  World = {
+        world : string
+    }
+
+    type HelloWorld = Hello & World
+
+    const helloWorld : HelloWorld = {
+        hello : "safin",
+        world : "mahi"
+    }
+
+    interface Num{
+        num : number
+    }
+
+    interface Str extends Num{
+        str : string
+    }
+
+    const numStr : Str = {
+        num : 32323,
+        str : "Hello World, I am hasibur rahman safin"
+    }
+
+
 
 
 
