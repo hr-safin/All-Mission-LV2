17850bad0e3b800046c19b34fa977b4b820f0c82
safinhr <hrsafin2434@gmail.com>
2024-08-15 23:07:42 +0600

    class

--- Mission 1/module3/src/practice4.ts
+++ Mission 1/module3/src/practice4.ts
@@ -1,6 +1,22 @@
 {
     // Class and object 
 
+    class Hello{
+        name : string;
+        age : number;
+        address : string;
+
+        constructor(name : string, age : number, address : string){
+            this.name = name
+            this.age = age
+            this.address = address
+        }
+
+        getBalance(){
+            console.log("Hello World")
+        }
+    }
+
 
 
 
