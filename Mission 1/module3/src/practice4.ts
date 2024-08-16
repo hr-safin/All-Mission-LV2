99efc90b365eb3f9fba8c487a7e022b8186128fc
safinhr <hrsafin2434@gmail.com>
2024-08-16 18:11:06 +0600

    class and object

--- Mission 1/module3/src/practice4.ts
+++ Mission 1/module3/src/practice4.ts
@@ -22,6 +22,36 @@
 
 
 
+    //Class and object
+
+    class User{
+        name : string
+        age : number
+        address : string
+
+        constructor(name : string, age : number, address : string){
+            this.name = name
+            this.age = age
+            this.address = address
+        }
+
+        hasCamera(){
+            console.log(`${this.name} has a wonderful camera`)
+        }
+    }
+
+    //Lets create an instance
+
+    const user1 = new User("Safin", 34, "Rampura Mohanagar project")
+
+    user1.name = "Hasibur Rahman Safin"
+    //console.log(user1)
+
+    //user1.hasCamera()
+
+
+
+
 
 
 
