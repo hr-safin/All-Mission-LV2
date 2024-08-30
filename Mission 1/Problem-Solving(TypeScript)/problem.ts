f31299344ce5cf5c2cc2f6e3547dfa2583d7e925
safinhr <hrsafin2434@gmail.com>
2024-08-30 22:09:38 +0600

    basic two problem solved

---
+++ Mission 1/Problem-Solving(TypeScript)/problem.ts
@@ -0,0 +1,16 @@
+
+// Basic Type
+
+//1 ---> Write a function add(a: number, b: number): number that takes two numbers and returns their sum.
+
+function sum(a : number, b : number) : number{
+   return a + b
+}
+//console.log(sum(12,23))
+
+//2 ---> Write a function greet(name: string): string that takes a string and returns a greeting message.
+
+function greet(name : string) : string {
+    return `hello, My name is ${name}`
+}
+// console.log(greet("Safin"))
