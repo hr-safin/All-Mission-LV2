2ee296768f5a2299c09980020b300139772c529b
safinhr <hrsafin2434@gmail.com>
2024-09-23 22:25:27 +0600

    problem 32 solved

--- Mission 1/Problem-Solving(TypeScript)/problem4.ts
+++ Mission 1/Problem-Solving(TypeScript)/problem4.ts
@@ -163,4 +163,24 @@ console.log(area1.getArea())
 
 
 
+class Counter{
+    static counter : number = 0
+
+   static increment(){
+       return Counter.counter = Counter.counter + 1
+   }
+
+   static decrement(){
+       return Counter.counter = Counter.counter - 1
+   }
+}
+
+
+const count1 = new Counter()
+
+console.log(Counter.increment()) // ---> Same Memory 
+console.log(Counter.increment())
+
+
+
 }
